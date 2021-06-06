0bd6f79cc61f5c4911b4c2949ffa7a6eef8ee351
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-06-06 21:28:54 +1000

    Creating a new article, CREATE step of CRUD. Handling with controllers new and create actions. On create, redirecting the browser to make a new request, else rendering the view for the current request.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
+  # resources comes from these following 2 lines. Article is considered a resource.
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 end
 
