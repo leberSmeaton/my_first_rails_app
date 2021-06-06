40ffd25ef2b902cae1d91efc9c17f967baedc041
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-06-06 23:32:47 +1000

    Generated a controller for Comments. Added a form for comments. Started refactoring by creating a _comment partial.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end
   # resources comes from these following 2 lines. Article is considered a resource.
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
